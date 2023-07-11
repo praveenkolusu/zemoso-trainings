b66e17a4fc9f4bed9da9279f846ca390a04139d7
praveenkolusu <praveen.kolusu@zemosolabs.com>
2023-07-11 13:28:47 +0530

    sonar-cloud integration

---
+++ minet/src/main/java/com/zmslabs/minet/MinetApplication.java
@@ -0,0 +1,13 @@
+package com.zmslabs.minet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinetApplication.class, args);
+	}
+
+}
---
+++ minet/src/test/java/com/zmslabs/minet/MinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmslabs.minet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
